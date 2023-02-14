b12a447a45a3b08bdc7af55a323b8a09389e5fd7
vitaliy <zndev@ya.ru>
2023-02-14 19:43:05 +0600

    upd

--- public/index.php
+++ public/index.php
@@ -1,7 +1,2 @@
 <?php
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG'], $context['APP_CONTEXT']);
-};
